a6b7c8683c957fed16f8fedb5da5be7c97201824
rutvi <rutvikpatel1432@gmail.com>
2026-03-06 00:12:40 -0500

    first committ

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3543c00c801c49c78a0d4e7ceafbe278c9c26f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "843980596" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "877963657" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
